117e6f11f39aebb1e38a204573167a45ef5e6b6c
imransarkerweb <imransarker.web@gmail.com>
2023-03-04 23:58:55 +0600

    code updated

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -1,18 +1,26 @@
 const linkContainer = document.getElementById("link-container")
 const urlCategory = 'https://openapi.programming-hero.com/api/news/categories'
 
-
-const getData = async (url) =>{
-    const data = await fetch(url)
-    const jsonData = await data.json();
-    displayCategory(jsonData.data)
-}
-
-getData(urlCategory)
+let url = 'https://openapi.programming-hero.com/api/news/category/01'
 
 const displayCategory = (data) => {
-   data.news_category.forEach(element => {
-    linkContainer.innerHTML += ` <li> <a class="text-decoration-none text-black" href="#">${element.category_name} </a></li>`
-   });
-    
+    data.news_category.forEach(element => {
+     linkContainer.innerHTML += ` <li> <a class="text-decoration-none text-black" href="#">${element.category_name} </a></li>`
+    });
+     
+ }
+
+const getData = async (url, display) =>{
+    const data = await fetch(url)
+    const jsonData = await data.json();
+    display(jsonData.data)
 }
+
+getData(urlCategory, displayCategory)
+
+// const displayCategory = (data) => {
+//    data.news_category.forEach(element => {
+//     linkContainer.innerHTML += ` <li> <a class="text-decoration-none text-black" href="#">${element.category_name} </a></li>`
+//    });
+    
+// }
