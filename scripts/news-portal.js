956057b7ddb2c0cc00872769ad277d6e451ebe30
imransarkerweb <imransarker.web@gmail.com>
2023-03-07 04:32:18 +0600

    code updated

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -6,24 +6,27 @@ let url = 'https://openapi.programming-hero.com/api/news/category/01'
 
 const displayCategory = (data) => {
     data.news_category.forEach(element => {
-     linkContainer.innerHTML += ` <li> <a class="text-decoration-none text-black" href="#">${element.category_name} </a></li>`
+      url = `https://openapi.programming-hero.com/api/news/category/${element.category_id}`
+     linkContainer.innerHTML += ` <li> <a class="text-decoration-none text-black" href="#" onclick="getData('${url}', displayNews)">${element.category_name} </a></li>`
     });
      
  }
 
  const displayNews = data =>{
     console.log(data)
+    cardContainer.innerHTML=''
     data.forEach(item =>{
         console.log(item)
+        
         cardContainer.innerHTML += `
         <div class="card my-3" >
         <div class="row g-0">
           <div class="col-md-4">
-          <img src="" class="img-fluid rounded-start" alt="...">
+          <img src="${item.image_url}" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">${item.title}</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
